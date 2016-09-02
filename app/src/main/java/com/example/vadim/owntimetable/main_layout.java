237981e34faf5d74222a5c6c4914c46457818e17
Vadim <ivadimdobrydnyk@gmail.com>
2016-09-02 23:32:14 +0300

    Initial commit

---
+++ app/src/main/java/com/example/vadim/owntimetable/main_layout.java
@@ -0,0 +1,17 @@
+package com.example.vadim.owntimetable;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by vadim on 9/2/16.
+ */
+public class main_layout extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
