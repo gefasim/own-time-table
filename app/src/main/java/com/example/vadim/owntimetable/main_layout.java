06362a676b9ef7e8c4a619d304a66f5b7d692699
gefasim <vov96now@gmail.com>
2016-09-22 18:10:04 +0300

    Garbage Collection

--- app/src/main/java/com/example/vadim/owntimetable/main_layout.java
+++ app/src/main/java/com/example/vadim/owntimetable/main_layout.java
@@ -6,6 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.LinearLayoutManager;
 
+import org.jsoup.Jsoup;
+
+import java.util.concurrent.ExecutionException;
+
 import Repositories.FakeRepository;
 import Repositories.IRepository;
 
@@ -19,6 +23,17 @@ public class main_layout extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+
+        HttpHtmlAsyncGetter ownapi = new HttpHtmlAsyncGetter();
+        String result = null;
+        try {
+            result = ownapi.execute().get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        HtmlParser localParser = new HtmlParser(Jsoup.parse(result));
+
+        listLessons = new CreateListLessons(localParser.getTimeTable());
 
         RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
         mRecyclerView.setHasFixedSize(true);
