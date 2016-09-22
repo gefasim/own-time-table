3723a1786383d25f2c4ab549daa54e91edca0f68
gefasim <vov96now@gmail.com>
2016-09-22 17:58:38 +0300

    Garbage Collection

---
+++ app/src/main/java/com/example/vadim/owntimetable/RecyclerAdapter.java
@@ -0,0 +1,57 @@
+package com.example.vadim.owntimetable;
+
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.vadim.owntimetable.Object.TimeTable_day;
+
+import java.util.List;
+
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+    private List<TimeTable_day> mDataset;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView mTextView;
+        public TextView mTextView2;
+
+        public ViewHolder(View v) {
+            super(v);
+            mTextView = (TextView) v.findViewById(R.id.tv_recycler_item);
+            mTextView2 = (TextView) v.findViewById(R.id.tv_recycler_item2);
+        }
+    }
+
+    public RecyclerAdapter(List<TimeTable_day> dataset) {
+        mDataset = dataset;
+    }
+
+    @Override
+    public RecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                         int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card, parent, false);
+
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+        holder.mTextView.setText(mDataset.get(position).getLesson_time());
+        holder.mTextView2.setText(mDataset.get(position).getLesson_name());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+}
--- app/src/main/java/com/example/vadim/owntimetable/main_layout.java
+++ app/src/main/java/com/example/vadim/owntimetable/main_layout.java
@@ -5,15 +5,6 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.LinearLayoutManager;
-import android.view.View;
-import android.widget.TextView;
-
-import com.example.vadim.owntimetable.Object.TimeTable_day;
-
-import org.jsoup.Jsoup;
-
-import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 import Repositories.FakeRepository;
 import Repositories.IRepository;
@@ -22,49 +13,22 @@ import Repositories.IRepository;
  * Created by vadim on 9/2/16.
  */
 public class main_layout extends AppCompatActivity {
-    private TextView mainTextView;
+
     CreateListLessons listLessons;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        IRepository repository = new FakeRepository();
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
 
-        mainTextView = (TextView)findViewById(R.id.mainTextView);
+        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
 
-        RecyclerView rv = (RecyclerView)findViewById(R.id.rv);
-        LinearLayoutManager llm = new LinearLayoutManager(getApplicationContext());
-        llm.setOrientation(LinearLayoutManager.VERTICAL);
-        rv.setLayoutManager(llm);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
 
-        OwnTimeAdapter adapter = new OwnTimeAdapter(repository.getCard());
-        rv.setAdapter(adapter);
-
-    }
-
-    public void onclick_TimeButton(View v) throws ExecutionException, InterruptedException {
-        HttpHtmlAsyncGetter ownapi = new HttpHtmlAsyncGetter();
-        String result = ownapi.execute().get();
-        HtmlParser localParser = new HtmlParser(Jsoup.parse(result));
-
-        Log.e("sass", "Sasha21");
-        Log.e("doc", String.valueOf(result.length()));
-        //Log.e("doc", result);
-
-
-        listLessons = new CreateListLessons(localParser.getTimeTable());
-        List<TimeTable_day> tempList =  listLessons.mainCreateList();   // print
-        String temp = "";                                               // print
-
-        for (TimeTable_day tableDay : tempList){
-            temp += tableDay.getLesson_time() + " ";
-            temp += tableDay.getLesson_name() + "\n";
-        }
-
-
-        mainTextView.setText( temp ); // print
-
+        RecyclerAdapter mAdapter = new RecyclerAdapter(listLessons.mainCreateList());
+        mRecyclerView.setAdapter(mAdapter);
 
     }
+
 }
